4d266af977c852b67586108354c2d90ae533f2c6
MrYassineNouri <mryassinenouri@gmail.com>
2024-02-29 10:33:13 +0100

    MAIN

---
+++ src/main/java/com/example/bbbbbbbb/BbbbbbbbApplication.java
@@ -0,0 +1,16 @@
+package com.example.bbbbbbbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
+public class BbbbbbbbApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BbbbbbbbApplication.class, args);
+
+	}
+
+}
